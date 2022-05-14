f74ccd0b0bf3dfc3c0864287798c80222ab02bb6
Wenwei Lin <wenwei.lin.me@gmail.com>
2022-05-14 11:16:31 +0800

    Prototype

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import { CssBaseline, StyledEngineProvider } from '@mui/material'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StyledEngineProvider injectFirst>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </StyledEngineProvider>
+  )
 }
 
 export default MyApp
